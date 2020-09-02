d73049fc8dc654fec1c0a31858c3457e23b2f650
prerna <prerna214@gmail.com>
2020-09-02 01:43:56 -0700

    Add a new route to get data from firestore

---
+++ api/routes/getDataAPI.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var router = express.Router();
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../serviceAccountKey.json");
+
+var db;
+
+router.get("/", function (req, res, next) {
+  db = admin.firestore();
+  //console.log(req.body);
+  db.collection("sampleData")
+    .get()
+    .then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+          console.log(doc.data());
+      });
+    })
+    .catch((err) => console.log(err));
+  res.send("Success! Data printed from firestore in server console!");
+});
+
+router.post("/", function (req, res, next) {
+    res.send("Printing data page ");
+  });
+
+module.exports = router;
